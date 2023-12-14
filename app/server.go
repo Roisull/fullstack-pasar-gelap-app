10d939ca4f3fe72969e425a03e8016de0b5a9150
Roisull <lusiro2001@students.ac.id>
2023-12-14 16:07:21 +0700

    membuat dan konfigurasi env file

--- app/server.go
+++ app/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +16,14 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) initialize() {
-	fmt.Println("Selamat datang di Pasar Gelap")
+type AppConfig struct {
+	AppName string
+	AppEnv string
+	AppPort string
+}
+
+func (server *Server) initialize(appConfig AppConfig) {
+	fmt.Println("Selamat datang di " + appConfig.AppName)
 
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
@@ -26,8 +34,25 @@ func (server *Server) Run(addr string){
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
 func Run(){
 	var server = Server{}
-	server.initialize()
-	server.Run(":9000")
+	appConfig := AppConfig{}
+
+	err := godotenv.Load()
+	if err != nil {log.Fatal("Error on load .env file")}
+
+	appConfig.AppName = getEnv("APP_NAME", "Pasar Gelap")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "9000")
+
+	server.initialize(appConfig)
+	server.Run(":" + appConfig.AppPort)
 }
