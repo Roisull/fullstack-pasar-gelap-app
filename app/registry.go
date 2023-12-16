3a9839db0495a3ae917f6bd78b082b8d05c2861d
Roisull <lusiro2001@students.ac.id>
2023-12-17 00:39:41 +0700

    membuat file registry model

---
+++ app/registry.go
@@ -0,0 +1,14 @@
+package app
+
+import "pasar-gelap/app/models"
+
+type Model struct {
+	Model interface{}
+}
+
+func RegisterModels() []Model {
+	return []Model{
+		{Model: models.User{}},
+		{Model: models.Address{}},
+	}
+}
