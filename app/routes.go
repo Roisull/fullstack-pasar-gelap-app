989f913710e3f70739814f3cba36f7773680005f
Roisull <lusiro2001@students.ac.id>
2023-12-17 00:41:58 +0700

    menambah dan mengubah hal hal kecil

--- app/routes.go
+++ app/routes.go
@@ -1,7 +1,12 @@
 package app
 
-import "pasar-gelap/app/controllers"
+import (
+	"pasar-gelap/app/controllers"
+
+	"github.com/gorilla/mux"
+)
 
 func (server *Server) initializeRoutes() {
+	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
 }
