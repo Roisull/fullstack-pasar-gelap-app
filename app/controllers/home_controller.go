1a8a5c58f74f6ca6403010e5445199c480d485c6
Roisull <lusiro2001@students.ac.id>
2023-12-19 00:53:49 +0700

    membuat persiapan home controller

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Selamat datang di Home Page pasar gelap")
+	data := map[string]string{
+		"message": "Selamat datang di Home Page pasar gelap",
+	}
+
+	json.NewEncoder(w).Encode(data)
 }
